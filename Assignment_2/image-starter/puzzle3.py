bdc89d50f2c64a656fe609d2be3c19f2bd249da2
Jiatian Li <cloudsweet1989@gmail.com>
2016-02-22 19:33:03 -0800

    Assignment_2 : finished puzzle3 with optimization

--- Assignment_2/image-starter/puzzle3.py
+++ Assignment_2/image-starter/puzzle3.py
@@ -1,8 +1,6 @@
 import sys
 from image import load_image, save_image, show_image, show_all_images, files_in_directory
 
-dict_cache = {}
-
 def calc_pixels_similarity(pixel0, pixel1):
 	"""
 	Calculate the similarity of two pixels, which is defined as
@@ -17,12 +15,11 @@ def calc_pixels_similarity(pixel0, pixel1):
 def calc_slices_similarity(slices, left, right):
 	"""
 	Calculate the similarity of two slices.
-	TODO: add border cache to avoid duplicate computation
 	"""
 	similarity = 0
+	# m is width
+	left_slice_m = len(slices[left])
 	for i in range(len(slices[left][0])):
-		# m is width
-		left_slice_m = len(slices[left])
 		left_pixel = slices[left][left_slice_m - 1][i]
 		right_pixel = slices[right][0][i]
 		similarity += calc_pixels_similarity(left_pixel, right_pixel)
@@ -30,7 +27,7 @@ def calc_slices_similarity(slices, left, right):
 
 def findBestMatch(slices):
 	"""
-	Find the best match in the given slices.
+	Find the best match with slice0 in the given slices.
 	Best match is defined as two slices with min sum of 
 	squares of the absolute difference between each color channel.
 	Return a tuple of (index_of_left_slice, index_of_right_slice)
@@ -38,17 +35,17 @@ def findBestMatch(slices):
 	left_index = 0
 	right_index = 0
 	best_similarity = sys.maxsize
-	for i in range(len(slices)):
-		for j in range(i + 1, len(slices)):
-			# slice i is left slice, slice j is right slice
-			similarity = calc_slices_similarity(slices, i, j)
-			if similarity < best_similarity:
-				left_index, right_index, best_similarity = i, j, similarity
-			# slice j is left slice, slice i is right slice
-			similarity = calc_slices_similarity(slices, j, i)
-			if similarity < best_similarity:
-				left_index, right_index, best_similarity = j, i, similarity
-			#print("best_similarity: {0}".format(best_similarity))
+
+	for i in range(1, len(slices)):
+		# slice 0 is left slice, slice i is right slice
+		similarity = calc_slices_similarity(slices, 0, i)
+		if similarity < best_similarity:
+			left_index, right_index, best_similarity = 0, i, similarity
+		# slice i is left slice, slice 0 is right slice
+		similarity = calc_slices_similarity(slices, i, 0)
+		if similarity < best_similarity:
+			left_index, right_index, best_similarity = i, 0, similarity
+		#print("best_similarity: {0}".format(best_similarity))
 	return left_index, right_index
 
 def merge(slices, left_index, right_index):
@@ -65,12 +62,9 @@ def main():
 	slices = []
 	for filename in filenames:
 		slices.append(load_image(filename))
-	print(len(slices))
+	#show_all_images(slices[0], tuple(slices[1:]))
 	while len(slices) > 1:
-		#show_all_images(slices[0], tuple(slices[1:]))
 		left_index, right_index = findBestMatch(slices)
-		#show_image(slices[left_index])
-		#show_image(slices[right_index])
 		print((left_index, right_index))
 		merge(slices, left_index, right_index)
 	show_image(slices[0])
